3971e932d1b56980eb6f2682835515094251f182
Gilmar Filho <gaxf12@gmail.com>
2016-07-07 22:28:20 -0300

    Correção do nome da classe

--- src/development/ExtrairAtividades.java
+++ src/development/ExtrairAtividades.java
@@ -5,49 +5,16 @@
  */
 package development;
 
-import java.io.File;
 import java.io.IOException;
-import org.apache.pdfbox.cos.COSDocument;
-import org.apache.pdfbox.io.RandomAccessFile;
-import org.apache.pdfbox.pdfparser.PDFParser;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
+
 /**
  *
  * @author Gilmar
  */
 public class ExtrairAtividades {
-    private PDFParser parser;
-    private PDFTextStripper pdfStripper;
-    private PDDocument pdDoc ;
-    private COSDocument cosDoc ;
-
-    private String Text ;
-    private String filePath;
-    private File file;
-    
-    public String ToText() throws IOException{
-        this.pdfStripper = null;
-        this.pdDoc = null;
-        this.cosDoc = null;
-
-        file = new File(filePath);
-        parser = new PDFParser(new RandomAccessFile(file,"r")); 
-
-        parser.parse();
-        cosDoc = parser.getDocument();
-        pdfStripper = new PDFTextStripper();
-        pdDoc = new PDDocument(cosDoc);
-        pdDoc.getNumberOfPages();
-        pdfStripper.setStartPage(1);
-        pdfStripper.setEndPage(10);
-
-        Text = pdfStripper.getText(pdDoc);
-        return Text;
-    }
-
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
-    }
-   
+    public static void main(String[] args) throws IOException {
+        PDFManager pdfManager = new PDFManager();
+        pdfManager.setFilePath("C:\\Users\\Gilmar\\Documents\\radoc-reader\\Trabalho-3-Exemplos-Radoc\\Radoc-2011-Final.pdf");
+        System.out.println(pdfManager.ToText());       
+    }  
 }
