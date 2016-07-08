e6c59b4113a983abeb0590651c4164e6ef22beb1
Gilmar Filho <gaxf12@gmail.com>
2016-07-07 22:09:53 -0300

    Criado as classes responsáveis por ler e imprimir o conteúdo do PDF

---
+++ src/development/PDFManager.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package development;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessFile;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+/**
+ *
+ * @author Gilmar
+ */
+public class PDFManager {
+    private PDFParser parser;
+    private PDFTextStripper pdfStripper;
+    private PDDocument pdDoc ;
+    private COSDocument cosDoc ;
+
+    private String Text ;
+    private String filePath;
+    private File file;
+    
+    public String ToText() throws IOException{
+        this.pdfStripper = null;
+        this.pdDoc = null;
+        this.cosDoc = null;
+
+        file = new File(filePath);
+        parser = new PDFParser(new RandomAccessFile(file,"r")); 
+
+        parser.parse();
+        cosDoc = parser.getDocument();
+        pdfStripper = new PDFTextStripper();
+        pdDoc = new PDDocument(cosDoc);
+        pdDoc.getNumberOfPages();
+        pdfStripper.setStartPage(1);
+        pdfStripper.setEndPage(10);
+
+        Text = pdfStripper.getText(pdDoc);
+        return Text;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+   
+}
---
+++ src/development/Reader.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package development;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Gilmar
+ */
+public class Reader {
+    public static void main(String[] args) throws IOException {
+        PDFManager pdfManager = new PDFManager();
+        pdfManager.setFilePath("C:\\Users\\Gilmar\\Documents\\radoc-reader\\Trabalho-3-Exemplos-Radoc\\Radoc-2011-Final.pdf");
+        System.out.println(pdfManager.ToText());       
+    }  
+}
