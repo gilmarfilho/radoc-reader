41dc075098a8d3ed41790bb2ac45bf75c4274a7c
Jorge Teixeira <jorgetbruno@hotmail.co.uk>
2016-07-09 18:04:24 -0300

    Criado metodo para extrair todos os dados de Atividades administrativas na classe administrativas

--- src/activities/Administrativas.java
+++ src/activities/Administrativas.java
@@ -6,6 +6,8 @@
 package activities;
 
 import data.ActivitiesReader;
+import java.text.ParseException;
+import java.util.ArrayList;
 
 /**
  *
@@ -18,6 +20,31 @@ public class Administrativas {
     public Administrativas(String radoc){
         this.activities = activitiesReader.extractActivities(radoc, "Atividades administrativas", "Produtos");
     }
+    public ArrayList<Activity> extractActivities() throws ParseException{
+        ArrayList<Activity> activities = new ArrayList<>();
+        
+        while(this.activities.indexOf("Descrição:") != -1){
+            //Descrição
+            String description = this.activitiesReader.extractData(this.activities, "Descrição:", "Órgão emissor");
+            //CHA
+            String cha = this.activitiesReader.extractData(this.activities, "CHA:", "Data início:");
+            //Data de início
+            String startDate = this.activitiesReader.extractDateField(this.activities, "Data início:");
+            //Data de término
+            String endDate = this.activitiesReader.extractDateField(this.activities, "Data término:");
+            //Removendo a atividade 1 da lista de atividades sem formatação
+            this.activities = this.activitiesReader.deleteData(this.activities, "Descrição:");
+            this.activities = this.activitiesReader.deleteData(this.activities, "Órgão emissor");
+            this.activities = this.activitiesReader.deleteData(this.activities, "CHA:");
+            this.activities = this.activitiesReader.deleteData(this.activities, "Data início:");
+            this.activities = this.activitiesReader.deleteData(this.activities, "Data término:");
 
+            //Adicionando a atividade na lista de atividades com formatação
+            Activity activity = new Activity(description, cha, startDate, endDate);
+            activities.add(activity);
+        }
+        
+        return activities;
+    }
 
 }
