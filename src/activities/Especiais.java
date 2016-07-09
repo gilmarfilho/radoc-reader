96880248fc3cb8caf806b3455614f027cc1d191c
Jorge Teixeira <jorgetbruno@hotmail.co.uk>
2016-07-09 18:33:10 -0300

    Criado metodo para extrair dados da classe Especiais

--- src/activities/Especiais.java
+++ src/activities/Especiais.java
@@ -7,6 +7,7 @@ package activities;
 
 import data.ActivitiesReader;
 import development.*;
+import java.text.ParseException;
 import java.util.ArrayList;
 
 /**
@@ -19,5 +20,31 @@ public class Especiais {
     
     public Especiais(String radoc){
         this.activities = activitiesReader.extractActivities(radoc, "Atividades acadêmicas especiais", "Atividades administrativas");
+    }
+    
+        public ArrayList<Activity> extractActivities() throws ParseException{
+        ArrayList<Activity> activities = new ArrayList<>();
+        
+        while(this.activities.indexOf("Tabela:") != -1){
+            //Descrição
+            String description = this.activitiesReader.extractData(this.activities, "Tabela:", "CHA");
+            //CHA
+            String cha = this.activitiesReader.extractData(this.activities, "CHA:", "Data início:");
+            //Data de início
+            String startDate = this.activitiesReader.extractDateField(this.activities, "Data início:");
+            //Data de término
+            String endDate = this.activitiesReader.extractDateField(this.activities, "Data término:");
+            //Removendo a atividade 1 da lista de atividades sem formatação
+            this.activities = this.activitiesReader.deleteData(this.activities, "Tabela:");
+            this.activities = this.activitiesReader.deleteData(this.activities, "CHA:");
+            this.activities = this.activitiesReader.deleteData(this.activities, "Data início:");
+            this.activities = this.activitiesReader.deleteData(this.activities, "Data término:");
+
+            //Adicionando a atividade na lista de atividades com formatação
+            Activity activity = new Activity(description, cha, startDate, endDate);
+            activities.add(activity);
+        }
+        
+        return activities;
     }
 }
