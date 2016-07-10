6620a34c9456487ba048503d9e7f92e9700149cc
Jorge Teixeira <jorgetbruno@hotmail.co.uk>
2016-07-09 22:30:33 -0300

    Commit parcial remoção de itens ensino

--- src/activities/Ensino.java
+++ src/activities/Ensino.java
@@ -9,6 +9,8 @@ import data.ActivitiesReader;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -24,6 +26,33 @@ public class Ensino {
 
     public ArrayList<Activity> extractActivities() throws ParseException{
         ArrayList<Activity> activities = new ArrayList<>();
+        String regex = "";
+            Pattern pattern;
+            Matcher matcher;
+            
+            //Descrição
+            String description = "";
+            regex = "ENGENHARIA DE COMPUTAÇÃO|ENGENHARIA DE SOFTWARE|CIÊNCIAS DA COMPUTAÇÃO|GESTÃO DA INFORMAÇÃO";
+            pattern = Pattern.compile(regex);
+            matcher = pattern.matcher(this.activities);
+            
+            if (matcher.find())
+            {
+                description = matcher.group(0);
+            }
+            System.out.println(description);
+            //CHA
+            String cha = "";
+            //Data de início
+            String startDate = "";
+            //Data de término
+            String endDate = "";
+         
+            //Adicionando a atividade na lista de atividades com formatação
+            Activity activity = new Activity(description, cha, startDate, endDate);
+            activities.add(activity);
+        
+        
         return activities;
     }
 }
